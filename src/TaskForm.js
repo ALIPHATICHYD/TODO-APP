f8de04e1532ff6d4d676f7a554841cc39afb8878
ALIPHATIC <adegokefuad1@gmail.com>
2023-06-13 17:47:18 +0100

    Handling Functions

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -1,19 +1,31 @@
-import {useState} from "react";
+import { useState } from "react";
 
-export default function TaskForm({onAdd}) {
-  const [taskName,setTaskName] = useState('');
+// TaskForm component
+export default function TaskForm({ onAdd }) {
+  // State for task name
+  const [taskName, setTaskName] = useState('');
+
+  // Function to handle form submission
   function handleSubmit(ev) {
     ev.preventDefault();
+    // Call onAdd function with taskName as argument
     onAdd(taskName);
+    // Reset taskName state to an empty string
     setTaskName('');
   }
+
+  // Rendered JSX
   return (
     <form onSubmit={handleSubmit}>
+      {/* Button to submit the form */}
       <button>+</button>
-      <input type="text"
-             value={taskName}
-             onChange={ev => setTaskName(ev.target.value)}
-             placeholder="Your next task..."/>
+      {/* Input field to enter task name */}
+      <input
+        type="text"
+        value={taskName}
+        onChange={ev => setTaskName(ev.target.value)}
+        placeholder="Your next task..."
+      />
     </form>
   );
 }
